3099a4289abb1b356340ab3783579756bf827392
Jack <hungjack1222@gmail.com>
2024-12-05 21:40:49 +0800

    feat: update main

--- TaikoSimulator/main.asm
+++ TaikoSimulator/main.asm
@@ -1,96 +1,304 @@
-.386
+.686P
+.XMM
 .model flat, c
+include csfml.inc
 
-EXTERN sfRenderWindow_create:PROC
-EXTERN sfRenderWindow_display:PROC
-EXTERN sfRenderWindow_clear:PROC
-EXTERN sfRenderWindow_destroy:PROC
-EXTERN sfRenderWindow_pollEvent:PROC
-EXTERN sfMusic_createFromFile:PROC
-EXTERN sfMusic_play:PROC
-EXTERN sfMusic_destroy:PROC
-
-sfVideoMode STRUCT
-	_width dd ?
-	height dd ?
-	bpp dd ?
-sfVideoMode ENDS
 .data
-    musicPath db "never-gonna-give-you-up-official-music-video.mp3", 0   ; 音樂檔案路徑
-    window_t byte "SFML Window", 0
-    ; 修改結構體初始化，正確給定每個成員
-    window_videoMode sfVideoMode <>
+    ; 檔案路徑
+    musicPath db "assets/main/v_title.ogg", 0
+    picPath db "assets/main/taiko_main.jpg", 0
+    fontPath db "assets/main/Taiko_No_Tatsujin_Official_Font.ttf", 0
+    
+    ; 視窗和遊戲標題
+    window_title db "Taiko Simulator", 0
+    ; 提示文字
+    prompt_string db "Click or Press Enter to Start", 0
+    
+    ; SFML物件
+    window dd 0
+    bgTexture dd 0
+    bgSprite dd 0
+    bgMusic dd 0
+    font dd 0
+    titleText dd 0
+    promptText dd 0
+    titleBounds sfFloatRect <>
+    textBounds sfFloatRect <>
+    
+    ; 視窗設定
+    window_videoMode sfVideoMode <1280, 720, 32>
+    window_realWidth dd 1280.0
+    ; 事件結構
+    event sfEvent <>
+    
+    ; 透明度相關
+    opacity dd 240.0
+    minOpacity dd 50.0
+    maxOpacity dd 240.0
+    deltaOpacity dd -0.05
+    
+    ; 顏色常數
+    titleColor sfColor <229, 109, 50, 255>
+    whiteColor sfColor <255, 255, 255, 255>
+    blackColor sfColor <0, 0, 0, 0>
+    redOutlineColor sfColor <255, 0, 0, 255>
 
-window DWORD ?
+    ; 位置常數
+    four dd 4.0
+    two dd 2.0
+    const_200 dd 200.0
 .code
 
-main PROC
-    ; This is jack branch
-   mov esi, esp ; test
-   push 0
-   push 6
-   push offset window_t
-   sub esp, 12
-   mov eax, esp
-   mov ecx, 800
-   mov [eax], ecx
-   mov ecx, 600
-   mov [eax+4], ecx
-   mov ecx, 32
-   mov [eax+8], ecx
+create_window PROC
+    push 0
+    push 6
+    push offset window_title
+    sub esp, 12
+    mov eax, esp
+    mov ecx, window_videoMode._width
+    mov [eax], ecx
+    mov ecx, window_videoMode.height
+    mov [eax+4], ecx
+    mov ecx, window_videoMode.bpp
+    mov [eax+8], ecx
     call sfRenderWindow_create
     add esp, 24
-   
-   mov window, eax
+    mov window, eax
+    ret
+create_window ENDP
 
-    ; 檢查是否成功創建視窗
-    test eax, eax
-    jz exitLoop                    ; 若返回值為 0，則視窗創建失敗，退出
+; 載入背景
+load_background PROC
+    ; 創建背景紋理
+    push 0
+    push offset picPath
+    call sfTexture_createFromFile
+    add esp, 8
+    mov bgTexture, eax
+    
+    ; 創建背景精靈
+    call sfSprite_create
+    mov DWORD PTR [bgSprite], eax
+    
+    ; 設定紋理
+    push 1
+    mov eax, DWORD PTR [bgTexture]
+    push eax
+    mov ecx, DWORD PTR [bgSprite]
+    push ecx
+    call sfSprite_setTexture
+    add esp, 12
+    ret
+load_background ENDP
 
-    mov ebx, eax                   ; 保存視窗指標
-
-    ; 加載音樂
-    push OFFSET musicPath
+play_music PROC
+    push offset musicPath
     call sfMusic_createFromFile
-    mov edi, eax  ; 保存音樂指標
+    add esp, 4 
+    mov bgMusic, eax
 
-    ; 播放音樂
-    push edi
+    push eax
     call sfMusic_play
+    add esp, 4
+    ret
+play_music ENDP
 
-    ; 主迴圈
-mainLoop:
-    ; 清空視窗
-    mov ebx, window
-    push ebx
-    call sfRenderWindow_clear
-    pop ebx
+; 設定標題文字
+setup_title_text PROC
+    ; Create font
+    push offset fontPath
+    call sfFont_createFromFile
+    add esp, 4
+    mov font, eax
+    
+    ; Create text object
+    call sfText_create
+    mov DWORD PTR [titleText], eax
+    
+    ; Set font
+    push font
+    mov eax, DWORD PTR [titleText]
+    push eax
+    call sfText_setFont
+    add esp, 8
+    
+    ; Set string
+    push offset window_title
+    mov eax, DWORD PTR [titleText]
+    push eax
+    call sfText_setString
+    add esp, 8
+    
+    ; Set character size
+    push 56
+    mov eax, DWORD PTR [titleText]
+    push eax
+    call sfText_setCharacterSize
+    add esp, 8
+    
+    ; Set fill color
+    push titleColor
+    mov eax, DWORD PTR [titleText]
+    push eax
+    call sfText_setFillColor
+    add esp, 8
+    
+    ; Set outline color
+    push whiteColor
+    mov eax, DWORD PTR [titleText]
+    push eax
+    call sfText_setOutlineColor
+    add esp, 8
+    
+    ; Set outline thickness
+    movss xmm0, DWORD PTR [four]  ; Load 4.0 into xmm0
+    sub esp, 4
+    movss DWORD PTR [esp], xmm0  ; Store xmm0 to memory
 
-    push ebx
-    ; 顯示視窗內容
-    call sfRenderWindow_display
-    pop ebx
+    push DWORD PTR [titleText]
 
-    ; 檢查視窗是否被關閉
-    call sfRenderWindow_pollEvent
-    cmp eax, 0   ; 假設0表示沒有事件
-    je mainLoop  ; 如果沒有事件，繼續迴圈
+    call sfText_setOutlineThickness
+    add esp, 8
+    
+    ; Set position
+    ;sub esp, 16
+    ;lea eax, [esp]
 
-    ; 如果有事件，檢查是否為關閉事件
-    cmp eax, 1   ; 假設1是關閉事件
-    ; je exitLoop
+    ;push DWORD PTR [titleText]
+    ;call sfText_getLocalBounds
 
-    ; 重新進入主迴圈
-    jmp mainLoop
+    ;mov eax, DWORD PTR [esp]
+    ;mov ebx, DWORD PTR [esp+4]
+    ;mov ecx, DWORD PTR [esp+8]
+    ;mov edx, DWORD PTR [esp+12]
 
-exitLoop:
-    ; 清理資源
-    push edi
-    call sfMusic_destroy
+    ;add esp, 16
+    
+    ; Adjust position
+    ;movss xmm0, DWORD PTR [window_realWidth]
+    ;subss xmm0, DWORD PTR [titleBounds._width]
+    ;movss xmm1, DWORD PTR [two]
+    ;divss xmm0, xmm1
+    ;movss DWORD PTR [esp-8], xmm0
+    
+    ;movss xmm0, DWORD PTR [const_200]
+    ;movss DWORD PTR [esp-4], xmm0
 
-    push ebx
-    call sfRenderWindow_destroy
+    ;push DWORD PTR [esp-4] ; y (200.0)
+    ;push DWORD PTR [esp-8] ; x (centered)
+    ;push DWORD PTR [titleText]
+    ;call sfText_setPosition
+    ;add esp, 12
+    
+    push 0
+    push 0
+    mov eax, DWORD PTR [titleText]
+    push eax
+    call sfText_setPosition
+    add esp, 12
 
     ret
+setup_title_text ENDP
+
+
+main PROC
+    ; 創建視窗
+    call create_window
+    test eax, eax
+    jz exit_program
+    
+   ; 載入背景
+    call load_background
+    test eax, eax
+    jz exit_program
+    
+    ; 播放音樂
+    call play_music
+    
+    ; 設定標題文字
+    call setup_title_text
+    
+    ; 設定提示文字
+    ;call setup_prompt_text
+
+main_loop:
+    ; 清除視窗
+    push blackColor
+    push window
+    call sfRenderWindow_clear
+    add esp, 8
+    
+    ; 繪製背景
+    push 0
+    mov eax, DWORD PTR [bgSprite]
+    push eax
+    mov ecx, DWORD PTR [window]
+    push ecx
+    call sfRenderWindow_drawSprite
+    add esp, 12
+
+    ; 繪製標題文字
+    push 0
+    mov eax, DWORD PTR [titleText]
+    push eax
+    mov ecx, DWORD PTR [window]
+    push ecx
+    call sfRenderWindow_drawText
+    add esp, 12
+    
+
+    ; 顯示視窗
+    mov eax, DWORD PTR [window]
+    push eax
+    call sfRenderWindow_display
+    add esp, 4
+
+    ; 事件處理
+    lea esi, event
+    push esi
+    push window
+    call sfRenderWindow_pollEvent
+    test eax, eax
+    jz main_loop
+    
+    ; 檢查關閉事件
+    cmp dword ptr [esi].sfEvent._type, sfEvtClosed
+    je exit_program
+    
+    ; 檢查滑鼠點擊
+    cmp dword ptr [esi].sfEvent._type, sfEvtMouseButtonPressed
+    je start_game
+    
+    ; 檢查鍵盤事件
+    cmp dword ptr [esi].sfEvent._type, sfEvtKeyPressed
+    je start_game
+    
+    jmp main_loop
+
+start_game:
+    ; 這裡可以添加進入音樂選擇頁面的邏輯
+    jmp main_loop
+
+exit_program:
+    ; 釋放資源
+    push window
+    call sfRenderWindow_destroy
+    add esp, 4
+
+    push bgMusic
+    call sfMusic_destroy
+
+    push bgSprite
+    call sfSprite_destroy
+    add esp, 4
+
+    push bgTexture
+    call sfTexture_destroy
+    add esp, 4
+
+    push titleText
+    call sfText_destroy
+    add esp, 4
 main ENDP
+
 END main
